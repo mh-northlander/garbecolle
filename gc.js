5eb32044a15d821d42b2e91599897353ce97c288
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2015-04-15 18:41:52 +0900

    duplicate urls

--- gc.js
+++ gc.js
@@ -446,7 +446,7 @@ function createGameoverScene(str){
         //game.replaceScene(createGameScene());
     });
 
-    var text = "#ガベこれ で" + score + "点を記録しました http://idylls.jp/~mh/garbagecollection/gc.html";
+    var text = "#ガベこれ で" + score + "点を記録しました";
     document.getElementById('tweetlink').href = "https://twitter.com/share?text=" + encodeURIComponent(text);
 
     return scene;
